4ff5caf3d04fca5488e48ec856a42550f8dc4509
dmitriy <dmitriy@singleton.ee>
2023-04-03 15:21:36 +0300

    chore: Add translation domain classifier values

---
+++ application/database/migrations/2023_04_03_113341_add_translation_domain_classifier_values.php
@@ -0,0 +1,116 @@
+<?php
+
+use App\Enums\ClassifierValueType;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        DB::table('classifier_values')->insert(
+            array_map(function (array $classifierValueData) {
+                return [
+                    ...$classifierValueData,
+                    'type' => ClassifierValueType::TranslationDomain->value,
+                    'meta' => '[]',
+                    'created_at' => DB::raw('NOW()')
+                ];
+            }, $this->getData())
+        );
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        DB::table('classifier_values')
+            ->whereIn('value', array_column($this->getData(), 'value'))
+            ->where('type', '=', ClassifierValueType::TranslationDomain->value)
+            ->delete();
+    }
+
+    private function getData(): array
+    {
+        return [
+            [
+                'name' => 'Haridus',
+                'value' => 'HAR',
+            ],
+            [
+                'name' => 'Teadus',
+                'value' => 'TEA',
+            ],
+            [
+                'name' => 'Arhiivindus',
+                'value' => 'ARH',
+            ],
+            [
+                'name' => 'Noorte- ja keelepoliitika',
+                'value' => 'NKP',
+            ],
+            [
+                'name' => 'Õiguspoliitika',
+                'value' => 'ÕIP',
+            ],
+            [
+                'name' => 'Kriminaalpoliitika',
+                'value' => 'KRP',
+            ],
+            [
+                'name' => 'Seadusetõlked',
+                'value' => 'SET',
+            ],
+            [
+                'name' => 'Justiitshalduspoliitika',
+                'value' => 'JHP',
+            ],
+            [
+                'name' => 'Eelarvepoliitika',
+                'value' => 'EAP',
+            ],
+            [
+                'name' => 'Maksu- ja tollipoliitika',
+                'value' => 'MTP',
+            ],
+            [
+                'name' => 'Riiklik statistika',
+                'value' => 'RST',
+            ],
+            [
+                'name' => 'Riigiraamatupidamine',
+                'value' => 'RRP',
+            ],
+            [
+                'name' => 'Finants- ja kindlustuspoliitika',
+                'value' => 'FKP',
+            ],
+            [
+                'name' => 'Kinnisvara- ja osaluspoliitika',
+                'value' => 'KOP',
+            ],
+            [
+                'name' => 'Avalik kord ja sisejulgeolek',
+                'value' => 'ASP',
+            ],
+            [
+                'name' => 'Kriisireguleerimine ja pästetööd',
+                'value' => 'KPT',
+            ],
+            [
+                'name' => 'Piirivalve',
+                'value' => 'PRV',
+            ],
+            [
+                'name' => 'Kodakondsuse, rände ja identiteedihaldus',
+                'value' => 'KRI',
+            ],
+            [
+                'name' => 'Rahvastiku- ja perepoliitika',
+                'value' => 'RPP',
+            ],
+        ];
+    }
+};
