6a0a06e0f359e09f397b2e90ec22a5257129e0c9
dmitriy <dmitriy@singleton.ee>
2023-04-03 15:37:45 +0300

    refactor: Remove generated comment

--- application/database/migrations/2023_04_03_122625_add_file_type_classifier_values.php
+++ application/database/migrations/2023_04_03_122625_add_file_type_classifier_values.php
@@ -21,9 +21,6 @@ return new class extends Migration
         );
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         DB::table('classifier_values')
