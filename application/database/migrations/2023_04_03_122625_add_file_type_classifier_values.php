d752c1c0288cb69875bd5efac65d3a7814e573eb
dmitriy <dmitriy@singleton.ee>
2023-04-03 15:37:03 +0300

    chore: Add file type classifier values

---
+++ application/database/migrations/2023_04_03_122625_add_file_type_classifier_values.php
@@ -0,0 +1,56 @@
+<?php
+
+use App\Enums\ClassifierValueType;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        DB::table('classifier_values')->insert(
+            array_map(function (array $classifierValueData) {
+                return [
+                    ...$classifierValueData,
+                    'type' => ClassifierValueType::FileType->value,
+                    'meta' => '[]',
+                    'created_at' => DB::raw('NOW()')
+                ];
+            }, $this->getData())
+        );
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        DB::table('classifier_values')
+            ->whereIn('value', array_column($this->getData(), 'value'))
+            ->where('type', '=', ClassifierValueType::FileType->value)
+            ->delete();
+    }
+
+    private function getData(): array
+    {
+        return [
+            [
+                'name' => 'Stiilijuhis',
+                'value' => 'SJ',
+            ],
+            [
+                'name' => 'Terminibaas',
+                'value' => 'TB',
+            ],
+            [
+                'name' => 'Abifail',
+                'value' => 'AF',
+            ],
+            [
+                'name' => 'Tõlkemälu',
+                'value' => 'TM',
+            ],
+        ];
+    }
+};
