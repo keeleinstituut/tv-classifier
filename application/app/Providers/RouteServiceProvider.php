d1fe401daf246c8d2265f891b88d4bc07fcd9142
Markus <Markus>
2023-06-28 11:12:52 +0300

    register web namespace routes

--- application/app/Providers/RouteServiceProvider.php
+++ application/app/Providers/RouteServiceProvider.php
@@ -30,6 +30,10 @@ class RouteServiceProvider extends ServiceProvider
             Route::middleware('api')
                 ->prefix('api')
                 ->group(base_path('routes/api.php'));
+
+            Route::middleware('web')
+                ->namespace($this->namespace)
+                ->group(base_path('routes/web.php'));
         });
     }
 
